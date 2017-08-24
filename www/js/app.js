84225e74926140aa8d5c38491813b21c82fe46f2
Jassive <jassivemarquez@hotmail.com>
2017-08-24 09:28:02 -0600

    Initial commit adding structure of directions and creating the first file

---
+++ www/js/app.js
@@ -0,0 +1,21 @@
+var base =5;
+var altura=7;
+var area = base * altura;
+console.log("El area de mi rectangulo es igual" + area);
+//Area de mi rectangulo es igual a 35
+
+var pi=3.1416;
+var radio=8;
+var area= radio^2*pi;
+console.log("El area de mi circulo es igual a" + area);
+//Area de mi circulo es igual a 27
+var ba=8;
+var al=10;
+var area=ba*al/2;
+console.log("El area de mi triangulo es igual a "  +area);
+//Area de mi traingulo es igual a 40 
+
+var pi=3.1416;
+var radio=6;
+var volumen=(4/3)*pi*radio^3;
+console.long("El volumen de mi esfera es igual a"    +volumen)
